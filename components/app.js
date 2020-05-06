76429f44245c4da3fb8892cef96d3a94fbf4339c
Jake Chustz <jake.chustz@gmail.com>
2020-05-06 16:47:06 -0700

    created start method, instantiated new app in main.js, and fixed url in get grades

--- components/app.js
+++ components/app.js
@@ -1,20 +1,26 @@
 class App{
-handleGetGradesError(error){
-console.error(error);
-}
-handleGetGradesSuccess(grades){
-  console.log(grades);
-}
-constructor(){
-  this.handleGetGradesError = this.handleGetGradesError.bind(this);
-  this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-}
-getGrades() {
-  $.ajax({
-    method: "GET",
-    url: "https://github.com/Learning-Fuze/sgt_api#get-all-grades",
-    success: this.handleGetGradesSuccess,
-    error: this.handleGetGradesError,
-  })
-}
-}
+  constructor(){
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error){
+  console.error(error);
+  }
+  handleGetGradesSuccess(grades){
+    console.log(grades);
+  }
+  getGrades(){
+    $.ajax({
+      method: "GET",
+      headers: {
+        "X-Access-Token": "WBDUJbdl"
+      },
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
+  }
+  start() {
+    this.getGrades();
+  }
+  }
