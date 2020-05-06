e6dbea8401a765a739195e56867af5c0f165ab8d
Jake Chustz <jake.chustz@gmail.com>
2020-05-06 15:55:25 -0700

    created methods in app class

--- components/app.js
+++ components/app.js
@@ -1,3 +1,20 @@
 class App{
-
+handleGetGradesError(error){
+console.error(error);
+}
+handleGetGradesSuccess(grades){
+  console.log(grades);
+}
+constructor(){
+  this.handleGetGradesError = this.handleGetGradesError.bind(this);
+  this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+}
+getGrades() {
+  $.ajax({
+    method: "GET",
+    url: "https://github.com/Learning-Fuze/sgt_api#get-all-grades",
+    success: this.handleGetGradesSuccess,
+    error: this.handleGetGradesError,
+  })
+}
 }
