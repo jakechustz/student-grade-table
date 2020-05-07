6edc1fbab40b5b0202b196f25db81076ac50deca
Jake Chustz <jake.chustz@gmail.com>
2020-05-07 12:51:08 -0700

    queried the doc for header element, and instantiated PageHeader with argument of header element

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var docTable = document.querySelector('table');
 var gradeTable = new GradeTable(docTable);
+var header = document.querySelector('header');
+var pageHeader = new PageHeader(header);
 var app = new App(gradeTable);
 app.start();
