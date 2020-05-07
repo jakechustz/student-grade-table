55ae3f761c02434a4e6997c1a4efd98255b71765
Jake Chustz <jake.chustz@gmail.com>
2020-05-07 12:53:05 -0700

    added pageHeader argument to instantiated app

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var docTable = document.querySelector('table');
 var gradeTable = new GradeTable(docTable);
 var header = document.querySelector('header');
 var pageHeader = new PageHeader(header);
-var app = new App(gradeTable);
+var app = new App(gradeTable, pageHeader);
 app.start();
