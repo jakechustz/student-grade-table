d18ff022fc89b576df34173f25fad44eeb66f886
Jake Chustz <jake.chustz@gmail.com>
2020-05-07 17:39:17 -0700

    revised grade table, getting error on line 36 about class dnone, will revisit Friday

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,22 +1,42 @@
 class GradeTable{
-  constructor(tableElement){
+  constructor(tableElement, noGradesElement){
     this.tableElement = tableElement;
+    this.noGradesEl = noGradesElement;
   }
+  renderGradeRow(data, deleteGrade) {
+    var tableRow = document.createElement('tr');
+    var nameElement = document.createElement('td');
+    var courseElement = document.createElement('td');
+    var gradeElement = document.createElement('td');
+    var operationsElement = document.createElement('td');
+    var deleteButton = document.createElement('button');
+
+    nameElement.textContent = data.name;
+    courseElement.textContent = data.course;
+    gradeElement.textContent = data.grade;
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'btn btn-danger'
+    deleteButton.addEventListener('click', function () { deleteGrade(data.id) });
+    operationsElement.append(deleteButton);
+    tableRow.append(nameElement, courseElement, gradeElement, operationsElement);
+    return tableRow;
+  }
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
+  }
+
   updateGrades(grades) {
     var tbody = this.tableElement.querySelector('tbody');
     tbody.textContent = " ";
-    for (var ii = 0; ii < grades.length; ii++) {
-      var tableRow = document.createElement('tr');
-      var nameEl = document.createElement('td');
-      var courseEl = document.createElement('td');
-      var gradeEl = document.createElement('td');
-
-      nameEl.textContent = grades[ii].name;
-      courseEl.textContent = grades[ii].course;
-      gradeEl.textContent = grades[ii].grade;
-
-      tableRow.append(nameEl, courseEl, gradeEl);
-      tbody.append(tableRow);
+    var noGradesEl = document.querySelector("#no-grades")
+    debugger;
+    if (!grades.length) {
+      this.noGradesEl.classList.remove('d-none');
+    } else {
+      this.noGradesEl.classList.add('d-none');
+    }
+    for (var i = 0; i < grades.length; i++) {
+    tbody.append(this.renderGradeRow(grades[i], this.deleteGrade));
     }
   }
 }
