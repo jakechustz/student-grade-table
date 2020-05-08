b55e8fb2a52a00d2fb0d32b4586de5fc57d9da91
Jake Chustz <jake.chustz@gmail.com>
2020-05-07 19:51:07 -0700

    created render grade row and updated updategrades with new for loop as well as add nogradesElement to constructor

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,22 +1,38 @@
 class GradeTable{
-  constructor(tableElement){
+  constructor(tableElement, noGradesElement){
     this.tableElement = tableElement;
+    this.noGradesElement = noGradesElement;
   }
   updateGrades(grades) {
     var tbody = this.tableElement.querySelector('tbody');
     tbody.textContent = " ";
     for (var ii = 0; ii < grades.length; ii++) {
-      var tableRow = document.createElement('tr');
-      var nameEl = document.createElement('td');
-      var courseEl = document.createElement('td');
-      var gradeEl = document.createElement('td');
+      tbody.append(this.renderGradeRow(grades[ii], this.deleteGrade));
+  }
+}
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
+  }
+  renderGradeRow(data, deleteGrade) {
+    var row = document.createElement("tr");
+    var studentName = document.createElement("td");
+    var studentCourse = document.createElement("td");
+    var studentGrade = document.createElement("td");
+    var deleteButton = document.createElement("td");
+    var button = document.createElement("button");
 
-      nameEl.textContent = grades[ii].name;
-      courseEl.textContent = grades[ii].course;
-      gradeEl.textContent = grades[ii].grade;
+    studentName.textContent = data.name;
+    studentCourse.textContent = data.course;
+    studentGrade.textContent = data.grade;
+    button.textContent = "DELETE";
+    button.className = "bun btn-danger";
 
-      tableRow.append(nameEl, courseEl, gradeEl);
-      tbody.append(tableRow);
-    }
+    deleteButton.append(button);
+    row.append(studentName, studentCourse, studentGrade, deleteButton);
+
+    button.addEventListener("click", function (event) {
+      deleteGrade(data.id);
+    })
+    return row;
   }
 }
